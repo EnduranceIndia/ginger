83958114109a6fd7991ef10ec3f1bf393d4ba48e
Joel Rosario <cirquitz@gmail.com>
2014-05-06 15:59:31 +0530

    The config structure is now defined outside get_conf so that it can be modified by tests.

--- config.rb
+++ config.rb
@@ -1,17 +1,17 @@
+CONF = {
+	'datasources' => {}
+}
+
+require "#{BASE}/config_#{ENV['RACK_ENV']}"
+
+env_conf(CONF)
+
+if CONF['datasources'].keys.length == 0
+	raise "At least one data sources must be configured."
+elsif !CONF.has_key?('base_files_directory')
+	raise "base_files_directory must be configured. It must contain subdirectories named 'pages' and 'cache', and the application must be able to create and read files in them."
+end
+
 def get_conf
-	conf = {
-		'datasources' => {}
-	}
-
-	require "#{BASE}/config_#{ENV['RACK_ENV']}"
-
-	env_conf(conf)
-
-	if conf['datasources'].keys.length == 0
-		raise "At least one data sources must be configured."
-	elsif !conf.has_key?('base_files_directory')
-		raise "base_files_directory must be configured. It must contain subdirectories named 'pages' and 'cache', and the application must be able to create and read files in them."
-	end
-
-	return conf
+	return CONF
 end
