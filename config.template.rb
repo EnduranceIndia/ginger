55f2ae1d07bb6afffdb184898e3c6155b0dfee99
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-17 18:30:54 +0530

    Update config.template.rb

--- config.template.rb
+++ config.template.rb
@@ -1,14 +1,21 @@
 def env_conf
 	{
-		'datasources' => {
-			'localhost' => {
-				'type' => 'mysql',
-				'hostname' => 'localhost',
-				'username' => 'user',
-				'password' => '12345'
-			}
-		},
+			'datasources' => {
+					'mysql-database' => {
+							'type' => 'mysql',
+							'hostname' => 'localhost',
+							'username' => 'root',
+							'password' => 's7a'
+					},
 
-		'base_files_directory' => '/1/2/3' # must contain subdirectories named 'pages' and 'cache'. The application must be able to read and write files in those directories
+					'postgres-database' => {
+							'type' => 'postgresql',
+							'hostname' => 'localhost',
+							'username' => 'postgres',
+							'password' => 's7a'
+					}
+			},
+
+			'base_files_directory' => 'files/' # must contain subdirectories named 'pages' and 'cache'. The application must be able to read and write files in those directories
 	}
 end
