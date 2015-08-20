cbd7fe5a507c7db0839fe39f22bf3924adab0be8
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-20 12:17:37 +0530

    Implement utilitites in data_source_utils

--- libs/data_source_utils.rb
+++ libs/data_source_utils.rb
@@ -3,23 +3,43 @@ def data_source
 end
 
 class DataSourceSQLiteStore < SQLiteStore
-  def load
-
+  def load(data_source_name)
+    data_source = db[:data_sources].where(data_source_name: data_source_name).first
+    if data_source
+    then
+      # Load the attributes
+      data_source_attributes = db[:data_source_attributes].where(data_source_name: data_source_name)
+      to_hash(data_source_attributes)
+    else
+      nil
+    end
   end
 
-  def to_hash
-
+  def to_hash(attributes)
+    attributes_hash = {}
+    attributes.each{|attr| attributes_hash[param_to_sym(attr[:attribute_name])] = attr[:attribute_value] }
+    attributes_hash
   end
 
-  def save
+  def save(data_source_name, attributes)
+    existing_data_source = load(data_source_name)
 
+    if existing_data_source != nil
+    then
+      db[:data_source_attributes].where(data_source_name: data_source_name).delete
+    else
+      db[:data_sources].insert(data_source_name: data_source_name)
+    end
+
+    attributes.each{|attr| db[:data_source_attributes].insert(data_source_name: data_source_name, attribute_name: attr[:name], attribute_value: attr[:value]) }
   end
 
   def list
 
   end
 
-  def delete
-
+  def delete(data_source_name)
+    db[:data_sources].where(data_source_name: data_source_name).delete
+    db[:data_source_attributes].where(data_source_name: data_source_name).delete
   end
 end
