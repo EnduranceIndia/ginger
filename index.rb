fe3aea9f89de7bed1ce0f585a1a487b0cdf8a5a2
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-20 18:05:57 +0530

    Fix broken help page

--- index.rb
+++ index.rb
@@ -309,213 +309,213 @@ class Ginger < Sinatra::Base
     redirect to("/page/#{page_id}")
   end
 
-  get '/help' do
+  get '/help', :auth => [:user] do
     help_text = <<-END
-	h2. Variables
+h2. Variables
 
-	Set the value of "var" to "b":
+Set the value of "var" to "b":
 
-	<pre>
-	<: var=b :></pre>
+<pre>
+<: var=b :></pre>
 
-	Display the value of "var":
+Display the value of "var":
 
-	<pre>
-	<:var:></pre>
+<pre>
+<:var:></pre>
 
-	h2. Case statement
+h2. Case statement
 
-	|_. Value of test "var" |_. Corresponding value of "var2" |
-	| a | 1 |
-	| b | 2 |
-	| c | 3 |
+|_. Value of test "var" |_. Corresponding value of "var2" |
+| a | 1 |
+| b | 2 |
+| c | 3 |
 
-	<br>
+<br>
 
-	<pre>
-	<: var=b :>
-	<:case:var:var2 (options=a,b,c values=1,2,3)</pre>
+<pre>
+<: var=b :>
+<:case:var:var2 (options=a,b,c values=1,2,3)</pre>
 
-	The test may refer to either a variable or a form parameter. If a variable and a form parameter exist by the same name, the variable is given preference. You can display the value of var2, which would be 2 in this case, using:
+The test may refer to either a variable or a form parameter. If a variable and a form parameter exist by the same name, the variable is given preference. You can display the value of var2, which would be 2 in this case, using:
 
-	<pre>
-	<:var2:></pre>
+<pre>
+<:var2:></pre>
 
-	h2. Tabular data
+h2. Tabular data
 
-	In the statement below, people_data is the data source being queried. The query "select * from people" is executed against this data source, and the result is displayed in tabular format. Tabular format is the default when the result is multiple rows or columns.
+In the statement below, people_data is the data source being queried. The query "select * from people" is executed against this data source, and the result is displayed in tabular format. Tabular format is the default when the result is multiple rows or columns.
 
-	<pre>
-	[:people_data select * from people :]</pre>
+<pre>
+[:people_data select * from people :]</pre>
 
-	In the statement below, the result has a single row and column, so the output will be rendered as plain text without any table markup.
+In the statement below, the result has a single row and column, so the output will be rendered as plain text without any table markup.
 
-	<pre>
-	[:people_data select count(*) from people :]</pre>
+<pre>
+[:people_data select count(*) from people :]</pre>
 
-	To explicitly choose plain text:
+To explicitly choose plain text:
 
-	<pre>
-	[:people_data:scalar select count(*) from people :]
-	</pre>
+<pre>
+[:people_data:scalar select count(*) from people :]
+</pre>
 
-	and for tabular output:
+and for tabular output:
 
-	<pre>
-	[:people_data:table select count(*) from people :]
-	</pre>
+<pre>
+[:people_data:table select count(*) from people :]
+</pre>
 
-	h2. Query expressions
+h2. Query expressions
 
-	<pre>
-	[:people_data select * from people {: where city='::city::' :}]</pre>
+<pre>
+[:people_data select * from people {: where city='::city::' :}]</pre>
 
-	The where clause will be added only if the city variable exists. The value of city will be escaped, because it is enclosed in double colons. Values are not escaped when enclosed in a single colons, like so:
+The where clause will be added only if the city variable exists. The value of city will be escaped, because it is enclosed in double colons. Values are not escaped when enclosed in a single colons, like so:
 
-	<pre>
-	[:people_data select * from people {: where age > :age: :}]</pre>
+<pre>
+[:people_data select * from people {: where age > :age: :}]</pre>
 
-	In the statement below, the where clauses will be added if city has been specified as a form parameter or variable.
+In the statement below, the where clauses will be added if city has been specified as a form parameter or variable.
 
-	<pre>
-	[:people_data select * from people {:city? where 1=2 :}]</pre>
+<pre>
+[:people_data select * from people {:city? where 1=2 :}]</pre>
 
-	To specify a data_source that is contained in a variable:
+To specify a data_source that is contained in a variable:
 
-	<pre>
-	<:ds_name=employee_ds:>
-	[:{:ds_name:} select * from people :]</pre>
+<pre>
+<:ds_name=employee_ds:>
+[:{:ds_name:} select * from people :]</pre>
 
-	This is useful when the data source needs to be changed based on some user specified input.
+This is useful when the data source needs to be changed based on some user specified input.
 
-	A case statement may be used to set a variable based on the value of the input, and that variable may be used as a data source:
+A case statement may be used to set a variable based on the value of the input, and that variable may be used as a data source:
 
-	<pre>
-	<:case:input:ds_name (options=1,2,3 ds_name=a,b,c) :>
-	[:{:ds_name:} select * from people :]</pre>
+<pre>
+<:case:input:ds_name (options=1,2,3 ds_name=a,b,c) :>
+[:{:ds_name:} select * from people :]</pre>
 
-	h2. Graphs
+h2. Graphs
 
-	<pre>
-	[:people_data:pie select city, count(*) from people group by city :]</pre>
+<pre>
+[:people_data:pie select city, count(*) from people group by city :]</pre>
 
-	The result set would look something like this:
+The result set would look something like this:
 
-	|_. Title |_. Value |
-	| Mumbai |  10 |
-	| Delhi | 20 |
-	| Bangalore | 30 |
+|_. Title |_. Value |
+| Mumbai |  10 |
+| Delhi | 20 |
+| Bangalore | 30 |
 
-	<br>
+<br>
 
-	In each row, the first column contains the title, and the second column contains corresponding value.
+In each row, the first column contains the title, and the second column contains corresponding value.
 
-	<pre>
-	[:people_data:bar (x_title='Some title' y_title='Some other title') select city, count(*) from people group by city :]</pre>
+<pre>
+[:people_data:bar (x_title='Some title' y_title='Some other title') select city, count(*) from people group by city :]</pre>
 
-	Bar and line charts are similar. x_title and y_title refer to the captions on the x and y axis, but are not compulsory.
+Bar and line charts are similar. x_title and y_title refer to the captions on the x and y axis, but are not compulsory.
 
-	h2. Forms
+h2. Forms
 
-	Forms allow the viewer of the page to supply values with which queries on the page may be parameterized. They are ideally declared at the top of the page. They must be described using the following syntax:
+Forms allow the viewer of the page to supply values with which queries on the page may be parameterized. They are ideally declared at the top of the page. They must be described using the following syntax:
 
-	<pre>
-	form. <:input:dropdown (name=country options=US,India,China values=us,india,cn title=Country) :></pre>
+<pre>
+form. <:input:dropdown (name=country options=US,India,China values=us,india,cn title=Country) :></pre>
 
-	A form declaration must be on a single line. The submit button at the end of the form declaration is at this time essential.
+A form declaration must be on a single line. The submit button at the end of the form declaration is at this time essential.
 
-	Here are the currently supported form fields.
+Here are the currently supported form fields.
 
-	h4. Dropdown
+h4. Dropdown
 
-	<pre>
-	<:input:dropdown (name=country options=US,India,China values=us,india,cn title=Country) :></pre>
+<pre>
+<:input:dropdown (name=country options=US,India,China values=us,india,cn title=Country) :></pre>
 
-	A dropdown will b displayed showing the options US, India, China, with corresponding values us, india and cn. When supplied by the user, they will be available in a variable named "country". The title of the dropdown will be displayed as "Country".
+A dropdown will b displayed showing the options US, India, China, with corresponding values us, india and cn. When supplied by the user, they will be available in a variable named "country". The title of the dropdown will be displayed as "Country".
 
-	A dropdown can also be specified in terms of a database query, like so:
+A dropdown can also be specified in terms of a database query, like so:
 
-	<pre>
-	[:people_data:dropdown (name=name title=Name option_column=name value_column=id) select * from people :]
-	</pre>
+<pre>
+[:people_data:dropdown (name=name title=Name option_column=name value_column=id) select * from people :]
+</pre>
 
-	In this example, the name column of the result is what will be displayed in the dropdown box, and the corresponding id column is the value sent back for this field when the query button is pressed.
+In this example, the name column of the result is what will be displayed in the dropdown box, and the corresponding id column is the value sent back for this field when the query button is pressed.
 
-	h4. Textbox
+h4. Textbox
 
-	<pre>
-	<:input:text (name=city title=City) :></pre>
+<pre>
+<:input:text (name=city title=City) :></pre>
 
-	h2. Text expressions
+h2. Text expressions
 
-	<pre>
-	<: city=mumbai :>
-	{: display this if :city: is specified :}</pre>
+<pre>
+<: city=mumbai :>
+{: display this if :city: is specified :}</pre>
 
-	This displays "display this if mumbai is specified" given that the value of "city" is "mumbai". But if no such variable exists, the entire expression is rendered as an empty string. "city" may also refer to a form parameter.
+This displays "display this if mumbai is specified" given that the value of "city" is "mumbai". But if no such variable exists, the entire expression is rendered as an empty string. "city" may also refer to a form parameter.
 
-	<pre>
-	{:city? display this if a city is specified :}</pre>
+<pre>
+{:city? display this if a city is specified :}</pre>
 
-	Displays "display this if a city is specified" if a form parameter or variable named city exists. If not, the entire expression is rendered as an empty string.
+Displays "display this if a city is specified" if a form parameter or variable named city exists. If not, the entire expression is rendered as an empty string.
 
-	h2. Side-by-side panel formatting for tables
+h2. Side-by-side panel formatting for tables
 
-	On the line before each table, put the following tag:
+On the line before each table, put the following tag:
 
-	<pre>
-	<:side_by_side:></pre>
+<pre>
+<:side_by_side:></pre>
 
-	After all the side-by-side tables, put this tag:
+After all the side-by-side tables, put this tag:
 
-	<pre>
-	<:side_by_side:end></pre>
+<pre>
+<:side_by_side:end></pre>
 
-	For example:
+For example:
 
-	<pre>
-	<:side_by_side:>
-	[:people_data select * from people {: where city='::city::'' :} :]
+<pre>
+<:side_by_side:>
+[:people_data select * from people {: where city='::city::'' :} :]
 
-	<:side_by_side:>
-	[:people_data select * from people {:city? where age > 20 :} :]
+<:side_by_side:>
+[:people_data select * from people {:city? where age > 20 :} :]
 
-	<:side_by_side:>
-	[:people_data select * from people {:city? where state='::state::' :} :]
+<:side_by_side:>
+[:people_data select * from people {:city? where state='::state::' :} :]
 
-	<:side_by_side:end:></pre>
+<:side_by_side:end:></pre>
 
-	h2. Conditional formatting
+h2. Conditional formatting
 
-	In the following table, any cell containing an age < 50 is highlighted with green, the rest with gray. The text is white.
+In the following table, any cell containing an age < 50 is highlighted with green, the rest with gray. The text is white.
 
-	The id column has been turned into a link using textile syntax, which is "Title" : http://url.com (without the spaces)
+The id column has been turned into a link using textile syntax, which is "Title" : http://url.com (without the spaces)
 
-	<pre>
-	[:people_data when id then format:'"id":http://go.to/%%' when age < 50 then background:green, text:white, bold when age >= 50 then background:gray, text:white select * from people :]</pre>
+<pre>
+[:people_data when id then format:'"id":http://go.to/%%' when age < 50 then background:green, text:white, bold when age >= 50 then background:gray, text:white select * from people :]</pre>
 
-	The syntax for specifying a condition and associated style looks like this: when column > value then style
+The syntax for specifying a condition and associated style looks like this: when column > value then style
 
-	These operators work on both numbers and words values: >, <, >=, <=, =, !=
+These operators work on both numbers and words values: >, <, >=, <=, =, !=
 
-	Numbers are compare ordinally, and words are compared in alphabetical order.
+Numbers are compare ordinally, and words are compared in alphabetical order.
 
-	Supported styles:
-	* bold, italics underline. Just use them like in the above example.
-	* Colors: may be specified as gray, green, etc. or in the #000, #000000 formats.
-	* Format: This is a primitive tool meant for replacing the value of a column with another value. The format must be specified in either single or double quotes. In the format template, %% is used to represent the column value.
+Supported styles:
+* bold, italics underline. Just use them like in the above example.
+* Colors: may be specified as gray, green, etc. or in the #000, #000000 formats.
+* Format: This is a primitive tool meant for replacing the value of a column with another value. The format must be specified in either single or double quotes. In the format template, %% is used to represent the column value.
 
-	h2. CSV format
+h2. CSV format
 
-	To display a single query in csv format:
+To display a single query in csv format:
 
-	* give the query an id. e.g. [:people_data (id=test_id) select * from people :]
-	* formulate the url like this: http://hostname:port/page/pagename?id=test_id&format=csv
+* give the query an id. e.g. [:people_data (id=test_id) select * from people :]
+* formulate the url like this: http://hostname:port/page/pagename?id=test_id&format=csv
 
-	If you wish to change the quote, line separator or format separator, use the parameters in the url below:
-	http://hostname:port/page/pagename?id=test_id&format=csv&col_separator=%7C&quote="&line_separator=$
+If you wish to change the quote, line separator or format separator, use the parameters in the url below:
+http://hostname:port/page/pagename?id=test_id&format=csv&col_separator=%7C&quote="&line_separator=$
 
-	This url specifies The line separator as $, and the quote as ". The column separator is given as %7C, which is the url encoded value of the | character. Not all symbols need to be encoded like this. It is necessary only if the application displays an error.
+This url specifies The line separator as $, and the quote as ". The column separator is given as %7C, which is the url encoded value of the | character. Not all symbols need to be encoded like this. It is necessary only if the application displays an error.
 
     END
 
