19b2724916b8476fa4512a059fced463c261d2c5
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-20 11:41:49 +0530

    Added a skeleton for the data source utils

---
+++ libs/data_source_utils.rb
@@ -0,0 +1,25 @@
+def data_source
+  DataSourceSQLiteStore.new
+end
+
+class DataSourceSQLiteStore < SQLiteStore
+  def load
+
+  end
+
+  def to_hash
+
+  end
+
+  def save
+
+  end
+
+  def list
+
+  end
+
+  def delete
+
+  end
+end
