6d2a6a279a7c10ea69d99b441533abd7b5d23f17
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-25 15:40:14 +0530

    Add utilities to convert permissions hash to string and vice versa

--- libs/common_utils.rb
+++ libs/common_utils.rb
@@ -6,3 +6,24 @@ end
 def escape(connection, value)
   "#{strip_quotes(connection.escape(value))}"
 end
+
+def permissions_hash_to_string(permissions_hash)
+  permissions_string = ''
+
+  permissions_hash.each do |name, permission|
+    permissions_string += "#{name}=#{permission};"
+  end
+
+  permissions_string
+end
+
+def permissions_string_to_hash(permissions_string)
+  permissions_hash = {}
+
+  permissions_string.split(';').each do |permission|
+    permission_literals = permission.split('=')
+    permissions_hash[param_to_sym(permission_literals.first)] = permission_literals.last
+  end
+
+  permissions_hash
+end
